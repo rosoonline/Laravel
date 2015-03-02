36abd8b216a5b1e414f6e0ff2a7797f486fff046
rosoonline <rossoonline@googlemail.com>
2015-03-02 22:03:19 +0000

    Order menu links in top dropdown

---
+++ app/Http/Controllers/OrdersController.php
@@ -0,0 +1,126 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// extras
+use App\Order;
+use Redirect;
+use Input;
+
+class OrdersController extends Controller {
+
+	 /**
+	* Create a new controller instance.
+	*
+	* @return void
+	*/
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		$month 			= str_pad(\Request::input('month', date('m')), 2, "0", STR_PAD_LEFT);
+		$year 			= \Request::get('year',date('Y'));
+
+		$orders 		= Order::where('date', '=', $year.'-'.$month.'-01')->get();
+		return view('orders.index', compact('orders'))->with('month', $month)->with('year', $year);
+		//return view('orders.index', array('orders' => $orders));
+	}
+	
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+	
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function import()
+	{
+		//$orders = Order::all();
+		return view('orders.import', compact('orders'));
+	}
+	
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function upload()
+	{
+		if (Order::upload()) {
+			return Redirect::route('dashboard')->with('message', 'Orders imported successfully');
+		} else {return Redirect::route('dashboard')->withErrors('Orders did not get imported');}
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
---
+++ app/Order.php
@@ -0,0 +1,51 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+// extras
+use DB;
+use PDO;
+use Goodby\CSV\Import\Standard\Lexer;
+use Goodby\CSV\Import\Standard\Interpreter;
+use Goodby\CSV\Import\Standard\LexerConfig;
+use Input;
+
+class Order extends Model {
+
+	protected function upload() {
+		
+		// remove any orders previously imported with the same year/month
+		$this->month 	= str_pad(Input::get('month'), 2, "0", STR_PAD_LEFT);
+		$this->year 	= Input::get('year');
+		DB::delete('DELETE from orders WHERE `date`=\''.$this->year.'-'.$this->month.'-00\'');
+		
+		// open PDO connection and do import
+		$pdo = new PDO('mysql:host='.$_ENV['DB_HOST'].';dbname='.$_ENV['DB_DATABASE'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+		
+		$config = new LexerConfig();
+		$lexer = new Lexer($config);
+
+		$interpreter = new Interpreter();
+		$interpreter->unstrict(); // Ignore row column count consistency
+
+		$interpreter->addObserver(function(array $columns) use ($pdo) {
+		
+			//$checkStmt = $pdo->prepare('SELECT count(*) FROM orders WHERE id = ?');
+			//$checkStmt->execute(array(($columns[0])));
+
+			//$count = $checkStmt->fetchAll()[0][0];
+			
+			//if ($count === '0') {
+				$stmt = $pdo->prepare('INSERT INTO orders (customer_code, sales, revenue, product, date, created_at) VALUES (?, ?, ?, ?, ?, ?)');
+				$columns[4] = $this->year.'-'.$this->month.'-01';
+				$columns[5] = date('Y-m-d H:i:s');
+				$stmt->execute($columns);
+			//}
+		});
+
+		$lexer->parse(Input::file('file'), $interpreter);
+		return true;
+		
+	}
+
+}
